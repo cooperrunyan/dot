3a6411ba4d44d736f38beb3ac755b730193c03d9
cooperrunyan <c@cooperrunyan.com>
2023-06-11 18:22:54 -0700

    Fixup setup script

--- setup.sh
+++ setup.sh
@@ -44,3 +44,68 @@ unset _submodule_path
 unset _submodule_url
 unset _submodule_name
 unset _submodule
+
+links=()
+folders=()
+moves=()
+
+echo " "
+echo "Creating symlinks:"
+echo " "
+
+SYM_BASE="$DIR/.config"
+
+for item in $SYM_BASE/{*,.*}; do
+  item_name=${item//$SYM_BASE\//}
+
+  test "$item_name" = '*' && continue
+  test "$item_name" = '.' && continue
+  test "$item_name" = '..' && continue
+  test "$item_name" = '.DS_Store' && continue
+
+
+  sym=$HOME/.config/${item//$SYM_BASE\//}
+
+  item=${item//\/\///}
+  sym=${sym//\/\///}
+
+  if [ -L "$sym" ]; then
+    rm -rf "$sym"
+  else
+    if [ -r "$sym" ]; then
+      moves+=("$sym")
+      mv "$sym" "$sym.old"
+    fi
+  fi
+  ln -sf "$item" "$sym"
+  links+=("~/${sym//$HOME\//}\\\\-->\\\\~/${item//$HOME\//}")
+done
+
+FOLDER_SPACE=0
+for folder in "${folders[@]}"; do
+  FOLDER_SPACE=1
+  echo "Made directory: ~/$folder/"
+done
+
+if [ $FOLDER_SPACE -eq 1 ]; then
+  echo " "
+fi
+
+MOVES_SPACE=0
+for sym in "${moves[@]}"; do
+  MOVES_SPACE=1
+  echo "~/${sym//$HOME\//} already existed (moved to ~/${sym//$HOME\//}.old)"
+done
+
+if [ $MOVES_SPACE -eq 1 ]; then
+  echo " "
+fi
+
+for link in "${links[@]}"; do
+  echo "Linked: ${link//\\/ }"
+done
+
+echo " "
+echo "Done setting up dotfiles"
+echo " "
+
